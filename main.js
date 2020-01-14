a424574c778b16f0684b4ef64cf0c996f2e1e785
Sebastian ICP <sebastian.icp@hotmail.com>
2020-01-14 12:35:42 -0300

    integrate-api

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 console.info("External script");
 
-const url = "http://localhost:3000/api/feriados";
+const url = "https://feriados-api.herokuapp.com/api/feriados";
 let resultados;
 
+const lista = document.getElementById('lista');
 
 document.addEventListener("DOMContentLoaded", () => (instance = M.AutoInit()));
 const toast = () => M.toast({ html: "I am a toast" });
+
 
 //peticion a la api
 axios.get(url)
@@ -15,4 +17,30 @@ axios.get(url)
   })
   .catch(function (error) {
     console.log(error);
+  })
+  .finally(()=>{
+    resultados.forEach(item => {
+      lista.innerHTML+=`
+      <div class="col s12 m6 l4">
+            <div class="card">
+              <div class="card-image">
+                <img src="img/Square_200x200.svg" />
+                <span class="card-title">${item.title}</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light red"
+                  ><i class="material-icons">add</i></a
+                >
+              </div>
+              <div class="card-content">
+                <p>
+                  Fecha: ${item.date}
+                </p>
+                <p>
+                  Tipo: ${item.extra}
+                </p>
+              </div>
+            </div>
+          </div>
+      
+      `;
+    });
   })
