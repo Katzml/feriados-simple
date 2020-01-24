331244aa2c1ee60de4bc49ddeb32a2f632564376
Sebastian ICP <sebastian.icp@hotmail.com>
2020-01-24 17:22:36 -0300

    fix-nav

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ const url = "https://feriados-api.herokuapp.com/api/feriados";
 let resultados;
 
 const lista = document.getElementById('lista');
+const preload = document.getElementById('preload');
 
 document.addEventListener("DOMContentLoaded", () => (instance = M.AutoInit()));
-const toast = () => M.toast({ html: "I am a toast" });
-
 
 //peticion a la api
 axios.get(url)
@@ -19,15 +18,14 @@ axios.get(url)
   })
   .finally(()=>{
     resultados.forEach(item => {
+      preload.style.display="none";
       lista.innerHTML+=`
       <div class="col s12 m6 l4">
             <div class="card">
               <div class="card-image">
                 <img class="lista-img" src="${item.image}" height="200" width="200"/>
-                <span class="card-title">${item.title}</span>
-                <a class="btn-floating halfway-fab waves-effect waves-light red"
-                  ><i class="material-icons">add</i></a
-                >
+                <span class="card-title"><h4 style="-webkit-text-stroke:1px black; margin-bottom:15%;">${item.title}</h4></span>
+                <!--<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>-->
               </div>
               <div class="card-content">
                 <p>
